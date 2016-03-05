e628aec3d9db99b7287622692283d6d217b6bbbf
Sebastian Lau <lauseb644@gmail.com>
2016-03-05 12:37:11 +0100

    ADD global header

---
+++ src/logcpp.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file logcpp.cpp
+ * @brief Global header file for LibLogC++
+ * @author Sebastian Lau <lauseb644 [at] gmail [dot] com>
+ **/
+/*
+	LibLogC++: A simple, intuitive and highly customizable LGPL library for logging with C++.
+	Copyright (C) 2015 Linux Gruppe IRB, TU Dortmund <linux@irb.cs.tu-dortmund.de>
+	Copyright (C) 2015-2016 Sebastian Lau <lauseb644@gmail.com>
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+	USA
+*/
+
+
+#include "channel_log.hpp"
+#include "log.hpp"
+#include "logcppversion.hpp"
+
